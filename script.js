1bf04fd6325a95e52b75e3f2399af81b0ea70593
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-07 20:19:08 -0300

    studied the arithmetic operations in Js

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-var a = true;
-console.log(a);
+// + adição
+// - subtração
+// * multiplicação
+// / divisão
+// % módulo
+// ++ incremento 
+// -- decremento
 
+/*var a = 10;
+var b = 15; 
 
-var b = null;
-console.log(b)
+var c = a + b; RESULTADO = 25
+var c = a - b; RESULTADO = -5
+var c = a * b; RESULTADO = 150
+var c = a / b; RESULTADO = 0.66666666
 
-var c = "string example";
-console.log(c)
+var c = a % b; RESULTADO = 10 
 
-var d = 21;
-console.log(d);
+AQUI DENTRO FOI ESTUDADO DE ADIÇÃO ATÉ O MÓDULO*/
 
-var e = 1;
+/* var a = 30;
+a ++; RESULTADO = 31
+a --; RESULTADO = 29
+AQUI FOI FEITO O USO MAIS SIMPLES DO INCREMENTO E DECREMENTO*/
 
-console.log("1" + "1");
-console.log(e + e);
-console.log(2+2);
+var a = 30;
+var b = ++a;
+
+/*var a = 30;
+var b = a--;
+AQUI VEMOS O USO ERRADO DE INCREMENTO/DECREMENTO QUE FOI DESCRITO NA MINHA ANOTAÇÃO NO WORD*/
+
+console.log(b);
