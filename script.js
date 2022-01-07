4fff60f1feed1187d5b26a0b6d317ab59fc131af
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-07 17:38:45 -0300

    here is an example of everything I studied at the types of data lesson

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-var a = 1;
-var b = "1"
+var a = true;
+console.log(a);
 
-console.log(a + a);
+
+var b = null;
+console.log(b)
+
+var c = "string example";
+console.log(c)
+
+var d = 21;
+console.log(d);
+
+var e = 1;
+
+console.log("1" + "1");
+console.log(e + e);
 console.log(2+2);
-console.log("2" + "2");
