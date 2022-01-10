a04f967e6b0ef8c271df37648a90ce6dade3e08c
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-10 11:13:29 -0300

    studied the comparison operators in Js

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-/* 
-=  x = y  x = y
-+=  x += y  x = x + y
--=  x -= y  x = x - y
-*=  x *= y  x = x * y
-/=  x /= y  x = x / y
-%=  x %= y  x = x % y
+/*
+    ------------OPERADORES DE COMPARAÇÃO------------
+   == igual a
+   === valor igual e igual
+   != não é igual
+   !== não igual ou não igual
+   > maior que 
+   < menor que
+   > = maior que ou igual a
+   <= menor ou igual a
 */
 
-var a = 5;
+//console.log(2==3); resultado: false
 
-a += 2;
-// a += 2; é igual a a = a + 2;
+/*
+var a = 2;
+var b =3;
+console.log(a == b);
+OS SINAIS TAMBÉM SERVEM PARA COMPARAR AS VARIÁVEIS COM OUTRAS VARIÁVEIS E 
+COM OUTROS VALORES!
+*/
 
-console.log(a);
+/*var a = 3;
+var b = "3";
+console.log(a===b);
+este compara não só o valor mas também o tipo, logo se os valores forem iguais
+mas de tipos diferentes, ele vai retornar false */
 
-/* learned how to use the atributions in Js!
-   the X would be my variable, and Y the value i'd like to add to it
-   it's only choose the operation and put it's sign in front of the =
-*/   
+/*var a = 3;
+var b = "3";
+console.log(a !== b); Levou em consideração o tipo entquanto que o != não leva!!!*/
+
+/*var a = 3;
+var b = 3;
+console.log(a > b); só compara se os valores são maiores ou menores, não compara se os
+valores são iguais*/
+
+var a = 3;
+var b = 3;
+console.log(a>=b);
