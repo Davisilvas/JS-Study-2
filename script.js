755b5099eb78fc4ce995ea83c836ee8c53ab2fa4
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-10 10:06:26 -0300

    learned the atributions in js

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
-// + adição
-// - subtração
-// * multiplicação
-// / divisão
-// % módulo
-// ++ incremento 
-// -- decremento
+/* 
+=  x = y  x = y
++=  x += y  x = x + y
+-=  x -= y  x = x - y
+*=  x *= y  x = x * y
+/=  x /= y  x = x / y
+%=  x %= y  x = x % y
+*/
 
-/*var a = 10;
-var b = 15; 
+var a = 5;
 
-var c = a + b; RESULTADO = 25
-var c = a - b; RESULTADO = -5
-var c = a * b; RESULTADO = 150
-var c = a / b; RESULTADO = 0.66666666
+a += 2;
+// a += 2; é igual a a = a + 2;
 
-var c = a % b; RESULTADO = 10 
+console.log(a);
 
-AQUI DENTRO FOI ESTUDADO DE ADIÇÃO ATÉ O MÓDULO*/
-
-/* var a = 30;
-a ++; RESULTADO = 31
-a --; RESULTADO = 29
-AQUI FOI FEITO O USO MAIS SIMPLES DO INCREMENTO E DECREMENTO*/
-
-var a = 30;
-var b = ++a;
-
-/*var a = 30;
-var b = a--;
-AQUI VEMOS O USO ERRADO DE INCREMENTO/DECREMENTO QUE FOI DESCRITO NA MINHA ANOTAÇÃO NO WORD*/
-
-console.log(b);
+/* learned how to use the atributions in Js!
+   the X would be my variable, and Y the value i'd like to add to it
+   it's only choose the operation and put it's sign in front of the =
+*/   
