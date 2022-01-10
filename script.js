18319440229f56accc9301241a6e980b9369d90a
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-10 19:58:18 -0300

    learned how to transform a string into an int or float

--- script.js
+++ script.js
@@ -1,61 +1,17 @@
-// && e
-// || ou
-// ! negação 
+var a = "3";
 
-/*
-    EXEMPLO BÁSICO DE &&
-var a = true;
-var b = false;
-var c = a&&b;
-console.log(c); RESULTADO FALSE!
+var b = "5";
 
-    EXEMPLO BÁSICO DE ||
-var a = true;
-var b = false;
-var c = a||b;
-console.log(c); RESULTADO TRUE!
+var c = parseInt(a) + parseInt(b);
 
-    EXEMPLO BÁSICO DE ! NEGAÇÃO
-var a = true;
-var b = !false;
-var c = a||b;
-console.log(c); RESULTADO TRUE!
-*/
+console.log(c)
 
-/*
-=============EXEMPLO COM O &&=============
-var idade = 20;
-var min = idade >= 20;
-var max = idade <=30;
-var pode = min && max;
+/* EXEMPLO COM FLOAT
+var a = "3.5";
 
-console.log("idade", idade)
-console.log("maior que 20?", min)
-console.log("menor que 30?", max)
-console.log("pode?", pode) 
-*/
+var b = "5";
 
+var c = parseFloat(a) + parseFloat(b);
 
-/*
-=============EXEMPLO COM ||=============
-var idade = 69;
-var menor10 = idade <= 10;
-var maior65 = idade >= 65;
-var grat = menor10 || maior65;
-
-console.log("idade", idade);
-console.log("é menor que 10 anos?", menor10);
-console.log("é maior que 65 anos?", maior65);
-console.log("tem direito a gratuidade?", grat);
-*/
-
-
-/*
-=============EXEMPLO COM NEGAÇÃO !=============
-var idade = 10;
-var maior = idade >= 20;
-var menor = !maior;
- 
-console.log ("maior que 20?", maior);
-console.log ("menor que 20?", menor);
+console.log(c)
 */
