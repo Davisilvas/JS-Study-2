61f930f4b0993ce01c033853e1b0c9acdf4b31a4
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-12 17:29:34 -0300

    the usage of function in Js pt1

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-/*
-PRIMEIRA FORMA DE SE DECLARAR UM ARRAY
-var alunos = new Array("Davi", "Maria Eduarda", "Igor", "Mufasa");
-
-console.log(alunos[1]);
-=========================
-*/
-
-//SEGUNDA FORMA DE SE DECLARAR UM ARRAY
-var alunos = ["Davi", "Maria Eduarda", "Igor", "Mufasa",
-"Patolino", "Pernalonga", "Jon Snow", "Han Solo"];
-
-
-for ( var i of alunos){
-
-    console.log(i);
+function media(n1, n2){
+    var nota1 = n1;
+    var nota2 = n2;
+    var m = (nota1 + nota2) /2;
+    //console.log (m);
+    return m;
 }
+
+// media(9, 8.5); também serveria para passar os valores
+var result1 = media(parseFloat(prompt("Insira a nota 1")), parseFloat(prompt("Insira a nota 2")));
+console.log(result1);
