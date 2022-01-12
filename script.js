3e65412290bd4090edea46e89001bd96411d8fc5
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-12 10:42:49 -0300

    learned the usage of for and while in Js

--- script.js
+++ script.js
@@ -1,34 +1,65 @@
-var n1 = parseFloat(prompt("Isira a primeira nota"));
-var n2 = parseFloat(prompt("Insira a segunda nota"));
-var m = (n1+n2) / 2 ;
-var conceito = ""
+/* 
+EXEMPLO MAIS SIMPLES DO USO DE FOR!!!!!!
+var numero = 5;
 
-if ( m >= 8.5){
-    conceito = "Ótimo";    
+for(var vez = 0; vez<numero; vez++){
+    console.log("executando");
 }
-else if (m >= 6.5){
-    conceito = "Bom";
-}    
-else{
-    conceito = "Regular";
+console.log("Acabou");
+*/ 
+/*
+var numero = parseInt(prompt("Insira um número para a repetição acontecer"));
+
+//for(var vez = 1; vez<=numero; vez++){ Dessa forma teria a contagem de 1 até 5 certinha.
+for(var vez = 0; vez<numero; vez++){
+
+    console.log("executando o for pela " + vez + " vez");
+}
+console.log("Acabou");
+=========================================
+*/
+
+/*EXEMPLO SIMPLES DO USO DE WHILE
+var numero = 5;
+
+while(numero < 10){
+    console.log("executando pela " + numero + " vez");
+    numero++;
+
+}
+console.log("acabou");
+=========================================
+*/
+
+/* EXEMPLO COM NÚMERO ALEATÓRIO
+var n = Math.random() * 100;
+
+while(n<=90){
+    console.log(n);
+    n = Math.random() * 100;
 }
 
-console.log(m);
-console.log(conceito);
+console.log(n + " opa, passou de 90!");
+console.log("acabou")
+=========================================
+*/
 
-//conceito = "vasco";
 
-switch(conceito){
+//EXEMPLO DO USO DE WHILE PRA VER OS MÚLTIPLOS!!
+var numero = 0;
 
-    case "Ótimo":
-        console.log("Parabéns, você é um ótimo aluno");
-        break;
-    case "Bom":
-        console.log("Parabéns você é um bom aluno e tem mais potencial a ser explorado");
-        break;
-    case "Regular":
-        console.log("Você precisa de um pouco mais de dedicação");
-        break;
-    default:
-        console.log("Houve algum erro");
+while(numero <= 100){
+    if( numero % 3 === 0 && numero % 5 ===0){
+        console.log (numero + " é múltiplo de 3 e 5");
+    } 
+    else if(numero % 5 ===0){
+        console.log(numero + " este número é múltiplo somente de 5");
+    }
+    else if(numero % 3 ===0){
+        console.log(numero + " este número é múltiplo somente de 3");
+    }
+    else{
+        console.log(numero + " este número não é múltiplo nem de 3 e nem de 5");
+    }
+    numero++;
 }
