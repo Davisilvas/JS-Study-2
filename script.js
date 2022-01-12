2844deb7cc45076e791016a795f636181cc68c9d
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-12 20:39:56 -0300

    usage of function in Js pt2

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
+/*
+EXEMPLO DE VARIÁVEL PASSANDO A REPRESENTAR A FUNÇÃO
 function media(n1, n2){
     var nota1 = n1;
     var nota2 = n2;
     var m = (nota1 + nota2) /2;
-    if (m>=7)
-        console.log("Parabéns você está a aprovado e sua média é " + m);
-    else
-        console.log("Estude mais, sua média foi " + m + " e você está de recuperação");    
+
     return m;
 }
-
-// media(9, 8.5); também serveria para passar os valores
 var result1 = media(parseFloat(prompt("Insira a nota 1")), parseFloat(prompt("Insira a nota 2")));
 console.log(result1);
+
+var mediaFinal = media;
+var result2 = mediaFinal(8, 7);
+console.log(result2);
+============================
+*/
+
+
+//var mf = (n1, n2) => {  ARROW FUNCTION
+var mf = function(n1, n2){
+
+    return (n1 + n2)/2
+}
+console.log(mf(8, 9));
