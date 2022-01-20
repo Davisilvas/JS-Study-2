75020fb00e3e2da1e7fdc8711a68bd4e86dd857c
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-20 17:17:32 -0300

    learned the usage of objects - methoods

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
+/*
+APRENDI UMA FORMA DE USAR A FUNÇÃO DENTRO DO OBJETO. SE ATENTAR QUE CASO FOR CHAMAR UMA PROPRIEDADE DO OBJETO, TEM QUE FAZER CERTO E NESSE CASO É "aluno.media(aluno.notas[0]...) SE NÃO FIZER ASSIM VAI DAR RUIM EM!!"
 var aluno = { nome: "Davi", 
               idade: 20,
-              notas: [9.0, 8.9]}
+              notas: [9.0, 8.9],
 
-//COMO CHAMAR UMA PROPRIEDADE DO OBJETO console.log(aluno.idade);
-//COMO CHAMAR O INDEX DE UM ARRAY NUM OBJETO console.log(aluno.notas[1]);
-//OUTRA FORMA DE CHAMAR UMA PROPRIEDADE console.log(aluno["nome"]);
-//SIMPLESMENTE CHAMANDO O OBJETO console.log(aluno);
-//console.log(aluno["notas"][1]);
+              media: function(n1, n2){
+                  if ((n1 + n2)/2 >= 7)
+                    return "APROVADO"
+                  else
+                    return "REPROVADO"  
+              }
 
-//aluno.matricula = 12344;
-//aluno["sobrenome"] = "Santos";
-//console.log(aluno.matricula);
-//console.log(aluno.sobrenome);
-
-var novaProp = "sobrenome"; 
-aluno[novaProp] = "Santos";
-console.log(aluno);
-
-var jogo = new Object();
-
-jogo["nome"] = "lol";
-console.log(jogo);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-FORMA DE USAR O ARRAY QUE EU NÃO TINHA PENSADO ANTES.
-var fff = [8, 9];
-var ddd = (fff[0] + fff[1])/2;
-console.log(ddd);
-
+            }
+ console.log(aluno.media(aluno.notas[0], aluno.notas[1]));
 */
+
+/*var calcMedia = function (n1, n2){
+    return (n1 + n2) /2    
+}*/
+/*var aluno1 = { nome: "Maria", 
+              idade: 23,
+              notas: [9.0, 10.0],  
+              media: function (){
+                return (this.notas[0] + this.notas[1]) /2
+              } 
+}*/
+
+
+function calcMedia (){
+    return (this.notas[0] + this.notas[1]) /2
+  } 
+
+var aluno = { nome: "Davi", 
+              idade: 20,
+              notas: [8.0, 7.9],
+              media: function (n1, n2){
+                return (n1 + n2) /2 
+              }           
+}
+
+var aluno1 = { nome: "Maria", 
+              idade: 23,
+              notas: [9.0, 10.0],  
+              media: calcMedia
+}
+
+
+console.log(aluno.nome);
+console.log(aluno.media(aluno.notas[0], aluno.notas[1]));
+
+
+console.log(aluno1.nome);
+console.log(aluno1.media());
+
+
