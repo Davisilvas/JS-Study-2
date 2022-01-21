8ff91fd51af5288a9b5d9b38f5cf27cbf691ceaf
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-20 21:33:26 -0300

    objeto

--- script.js
+++ script.js
@@ -1,56 +1,56 @@
-/*
-APRENDI UMA FORMA DE USAR A FUNÇÃO DENTRO DO OBJETO. SE ATENTAR QUE CASO FOR CHAMAR UMA PROPRIEDADE DO OBJETO, TEM QUE FAZER CERTO E NESSE CASO É "aluno.media(aluno.notas[0]...) SE NÃO FIZER ASSIM VAI DAR RUIM EM!!"
-var aluno = { nome: "Davi", 
-              idade: 20,
-              notas: [9.0, 8.9],
+/*ARRAY COM VÁRIAS CHAVES DENTRO
+var calcMedia = function(){
+    return (this.nota1 + this.nota2) / 2;
+}
 
-              media: function(n1, n2){
-                  if ((n1 + n2)/2 >= 7)
-                    return "APROVADO"
-                  else
-                    return "REPROVADO"  
-              }
-
+var turma = [
+            {
+                nome: "Davi",
+                nota1: 7.5,
+                nota2: 8.8,
+                media: calcMedia
             }
- console.log(aluno.media(aluno.notas[0], aluno.notas[1]));
-*/
+]
 
-/*var calcMedia = function (n1, n2){
-    return (n1 + n2) /2    
+var aluno = turma[0];
+console.log(aluno);
+console.log(aluno.media());*/
+
+// MELHOR FORMA DE SE FAZER UM ARRAY DE OBJETOS (PRA ESSE CASO). FOI FEITO ATRAVÉS DO USO DE UMA FUNÇÃO!!!
+
+/*function criarAluno(nome, n1, n2){
+    return   {
+        nome: nome,
+        nota1: n1,
+        nota2: n2,
+        media: function(){
+            return (this.nota1 + this.nota2) / 2;
+            }
+        }
 }*/
-/*var aluno1 = { nome: "Maria", 
-              idade: 23,
-              notas: [9.0, 10.0],  
-              media: function (){
-                return (this.notas[0] + this.notas[1]) /2
-              } 
-}*/
-
-
-function calcMedia (){
-    return (this.notas[0] + this.notas[1]) /2
-  } 
-
-var aluno = { nome: "Davi", 
-              idade: 20,
-              notas: [8.0, 7.9],
-              media: function (n1, n2){
-                return (n1 + n2) /2 
-              }           
+ 
+function aluno(nome, n1, n2){
+    this.aluno = nome;
+    this.nota1 = n1;
+    this.nota2 = n2; 
+    this.media = function media(){
+        return (this.nota1 + this.nota2) / 2
+    }
 }
+var a = new aluno("Davi", 9, 8);
+console.log(a); 
+console.log(a.media())
+//var turma = [
+//    criarAluno("Davi", 8.0, 7.7),
+//    criarAluno("Maria", 9.2, 8.9),
+//    criarAluno("Doly", 10.0, 9.6)
+//]
 
-var aluno1 = { nome: "Maria", 
-              idade: 23,
-              notas: [9.0, 10.0],  
-              media: calcMedia
-}
-
-
-console.log(aluno.nome);
-console.log(aluno.media(aluno.notas[0], aluno.notas[1]));
-
-
-console.log(aluno1.nome);
-console.log(aluno1.media());
+//for (var aluno of turma){
+  //  console.log(aluno)
+//}
 
 
+//turma.forEach(function(elemento){
+//    console.log(elemento);
+//})
