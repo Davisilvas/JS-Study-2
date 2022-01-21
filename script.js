dcf62f5dbb5f98072fa1daef81d0def8077d2af0
Davi Silva Santos <morg.daviss@gmail.com>
2022-01-21 19:57:49 -0300

    objects again

--- script.js
+++ script.js
@@ -1,61 +1,14 @@
-/*ARRAY COM VÁRIAS CHAVES DENTRO
-var calcMedia = function(){
-    return (this.nota1 + this.nota2) / 2;
-}
-
-var turma = [
-            {
-                nome: "Davi",
-                nota1: 7.5,
-                nota2: 8.8,
-                media: calcMedia
-            }
-]
-
-var aluno = turma[0];
-console.log(aluno);
-console.log(aluno.media());*/
-
-// MELHOR FORMA DE SE FAZER UM ARRAY DE OBJETOS (PRA ESSE CASO). FOI FEITO ATRAVÉS DO USO DE UMA FUNÇÃO!!!
-
-/*function criarAluno(nome, n1, n2){
-    return   {
-        nome: nome,
-        nota1: n1,
-        nota2: n2,
-        media: function(){
-            return (this.nota1 + this.nota2) / 2;
-            }
-        }
-}*/
- 
-function aluno(nome, n1, n2){
-    this.aluno = nome;
-    this.nota1 = n1;
-    this.nota2 = n2; 
-    this.media = function media(){
-        return (this.nota1 + this.nota2) / 2
-    }
-}
-var a = new aluno("Davi", 9, 8);
-var b = new aluno("Maria", 10, 8.9);
-
-
-console.log(a); 
-console.log(a.media())
-console.log(b); 
-console.log(b.media())
-//var turma = [
-//    criarAluno("Davi", 8.0, 7.7),
-//    criarAluno("Maria", 9.2, 8.9),
-//    criarAluno("Doly", 10.0, 9.6)
-//]
-
-//for (var aluno of turma){
-  //  console.log(aluno)
+//function obj(n, s){
+//        return {nome: n, sobrenome: s}
 //}
+//
+//var a = obj("Neymar", "Júnior");
+//console.log(a);
 
+function obj(n , s){
+    this.nome = n;
+    this.sobrenome = s;
+}
 
-//turma.forEach(function(elemento){
-//    console.log(elemento);
-//})
+var a = new obj("Neymar", "Júnior");
+console.log(a);
